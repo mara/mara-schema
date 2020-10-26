1d614878a0d03c65772ef93ea11c57e762895fb8
ice1e0 <ls@ice1e0.com>
2020-10-26 22:23:39 +0100

    fix missing ui folder (#6)

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import pathlib
 from setuptools import setup, find_packages
 import re
 
 def get_long_description():
     with open('README.md') as f:
         return re.sub('!\[(.*?)\]\(docs/(.*?)\)', r'![\1](https://github.com/mara/mara-schema/raw/master/docs/\2)', f.read())
+
+def static_files() -> [str]:
+    module_path = pathlib.Path('mara_schema')
+    files = []
+    for p in module_path.glob('ui/static/**/*'):
+        if p.is_file():
+            files.append(str(p.relative_to(module_path)))
+    return files
+
 
 setup(
     name='mara-schema',
@@ -24,6 +34,7 @@ setup(
     python_requires='>=3.6',
 
     packages=find_packages(),
+    package_data={'mara_schema': static_files()},
 
     author='Mara contributors',
     license='MIT',
