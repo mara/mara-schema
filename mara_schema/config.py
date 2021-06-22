68f5f8643f40c68dfa1a79ce32f5ef1e15a15c64
Saman <sam.an@outlook.de>
2021-06-22 17:37:43 +0200

    Add feature to return all entities
    Similarly as how to return all datasets, this enables to return all entities.

--- mara_schema/config.py
+++ mara_schema/config.py
@@ -1,8 +1,15 @@
 import functools
 from .data_set import DataSet
+from .entity import Entity
 
 @functools.lru_cache(maxsize=None)
 def data_sets() -> [DataSet]:
     """Returns all available data_sets."""
     from .example import example_data_sets
     return example_data_sets()
+
+@functools.lru_cache(maxsize=None)
+def entities() -> [Entity]:
+    """Returns all available entities."""
+    from .example import example_entities
+    return example_entities()
--- mara_schema/example/__init__.py
+++ mara_schema/example/__init__.py
@@ -1,8 +1,16 @@
 from ..data_set import DataSet
-
+from ..entity import Entity
 
 def example_data_sets() -> [DataSet]:
     from .data_sets.customers import customers_data_set
     from .data_sets.order_items import order_items_data_set
     from .data_sets.products import products_data_set
     return [order_items_data_set, customers_data_set, products_data_set]
+
+def example_entities() -> [Entity]:
+    from .entities.customer import customer_entity
+    from .entities.order import order_entity
+    from .entities.order_item import order_item_entity
+    from .entities.product import product_entity
+    from .entities.product_category import product_category_entity
+    return [customer_entity, order_entity, order_item_entity, product_entity, product_category_entity]
